8e282e8a2e6cf8c7fe2045ed27f877f3c34d768d
cutsea110 <cutsea110@gmail.com>
2022-03-15 12:04:20 +0900

    share db

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // リスナーをこのアドレスにバインドする
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         // タプルの2つ目の要素は、新しいコネクションのIPとポートの情報を含んでいる
         let (socket, _) = listener.accept().await.unwrap();
+
+        // db を複製する
+        let db = Arc::clone(&db);
+
+        println!("Accepted");
         // それぞれのインバウンドソケットに対して新しいタスクを spawn する
         // ソケットは新しいタスクに move され、そこで処理される
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // データを蓄えるため HashMap を使う
-    let mut db = HashMap::new();
 
     // mini-redis が提供するコネクションによって、ソケットからくるフレームをパースする
     let mut connection = Connection::new(socket);
@@ -31,11 +40,13 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
+                let mut db = db.lock().unwrap();
                 // Vec<u8> として保存する
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     // Frame::Bulk はデータが Bytes 型であることを期待する
                     // ここでは &Vec<u8> から Bytes に変換する
